d848073321f7b8604e230e9db714a7d7ed838624
arunacharvkd <arunacharvkd@gmail.com>
2021-08-25 20:26:05 +0530

    test

---
+++ src/pages/Homepage/homepage.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+const HomePage = () => {
+  return (
+    <div>test 2</div>
+  )
+    
+}
+
+export default HomePage
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import HomePage from './Homepage/homepage'
+
+const IndexPage = () => {
+  return (
+    <div><HomePage/></div>
+  )
+    
+}
+
+export default IndexPage
